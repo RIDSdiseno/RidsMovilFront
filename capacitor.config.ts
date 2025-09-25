157624dfbef3651e01bd7c4e6437caeba12305ef
Mario <>
2025-09-25 16:55:08 -0300

    arreglo

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.ridsmovilfront',
-  appName: 'ridsmovilfront',
-  webDir: 'www'
-};
-
-export default config;
